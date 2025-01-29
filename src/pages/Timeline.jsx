17dd7a316580d3212ba9ec76388364b8704703c3
aditikansal01 <kansaladiti70@gmail.com>
2025-01-29 02:27:08 -0800

    Update Timeline.jsx: Enhanced event descriptions

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -5,41 +5,55 @@ import Snowfall from 'react-snowfall';
 const Timeline = () => {
   const events = [
     {
-      title: "Coming Soon",
-      date: "Feb 1st to 2nd 2025",
-      time: "XX:00 PM",
-      description: "Coming Soon",
-      location: "JIIT"
+      title: "Opening Ceremony",
+      date: "Feb 1st 2025",
+      time: "1:00 PM - 4:00 PM",
+      description: "Welcome to Xenith 2025! Join us for an exciting inauguration featuring keynote speakers, event roadmap unveiling, and networking opportunities with tech enthusiasts.",
+      
     },
     {
-      title: "Coming Soon",
-      date: "Feb 1st to 2nd 2025",
-      time: "XX:00 PM",
-      description: "Coming Soon",
-      location: "JIIT"
+      title: "Codeascend",
+      date: "Feb 1st 2025",
+      time: "4:00 PM - 6:30 PM",
+      description: " The ultimate challenge of competitive programming! Driven by GeeksforGeeks, CodeAscend ignites the stage with an exciting mini-challenge.",
+      
     },
     {
-      title: "Coming Soon",
-      date: "Feb 1st to 2nd 2025",
-      time: "XX:00 PM",
-      description: "Coming Soon",
-      location: "JIIT"
-    },
-    {
-      title: "Coming Soon",
-      date: "Feb 1st to 2nd 2025",
-      time: "XX:00 PM",
-      description: "Coming Soon",
-      location: "JIIT"
-    },
-    {
-      title: "Coming Soon",
-      date: "Feb 1st to 2nd 2025",
-      time: "XX:00 PM",
-      description: "Coming Soon",
-      location: "Venue : JIIT"
+      title: "Hackstreet",
+      date: "Feb 1st 2025",
+      time: "12:00 PM onwards",
+      description: "The ultimate 24-hour online hackathon open to colleges across the nation.",
+    
     },
 
+    {
+      title: "Shardrift",
+      date: "Feb 2nd 2025",
+      time: "10:00 AM - 1:00 PM",
+      description: "An extravaganza of wit, courage, and camaraderie.",
+      
+    },
+    {
+      title: "Electrosphere",
+      date: "Feb 2nd 2025",
+      time: "2:00 PM - 4:00 PM",
+      description: " A three level tournament designed to test your mettle and stretch your knowledge to its arctic extremes, culminating in a charging battle of wits.",
+    
+    },
+    {
+      title: "IdeateX",
+      date: "Feb 2nd 2025",
+      time: "2:00 PM - 4:00 PM",
+      description: "An innovative Startup Pitch Competition designed for visionaries.",
+     
+    },
+    {
+      title: "Closing Ceremony",
+      date: "Feb 2nd 2025",
+      time: "4:00 PM - 7:00 PM",
+      description: "Join us for the grand finale with prize distribution and celebration of this year's achievements. Network one last time before we wrap up!",
+      
+    }
   ];
 
   return (
